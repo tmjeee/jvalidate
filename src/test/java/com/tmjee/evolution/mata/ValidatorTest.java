c157c36098e49f7e2c0037af11ef22cd9820916e
tmjee1 <tmjee1@gmail.com>
2016-01-31 20:22:18 +1100

    tests

--- src/test/java/com/tmjee/evolution/mata/ValidatorTest.java
+++ src/test/java/com/tmjee/evolution/mata/ValidatorTest.java
@@ -34,14 +34,57 @@ public class ValidatorTest {
     }
 
     @Test
+    public void test_lessThan() throws Exception {
+        Validator v = Validators.lessThan(5);
+        v.validate("a", "a", mockResolver);
+        v.validate("1", "b", mockResolver);
+        v.validate("6", "c", mockResolver);
+        v.validate("5", "d", mockResolver);
+
+        verify(mockResolver, times(1)).registerError("a", "a is not a number");
+        verify(mockResolver, times(1)).registerError("c", "c is not less than 5");
+        verify(mockResolver, times(1)).registerError("d", "d is not less than 5");
+        verifyNoMoreInteractions(mockResolver);
+    }
+
+    @Test
+    public void test_lessThanOrEquals() throws Exception {
+        Validator v = Validators.lessThanOrEquals(5);
+        v.validate("a", "a", mockResolver);
+        v.validate("1", "b", mockResolver);
+        v.validate("6", "c", mockResolver);
+        v.validate("5", "d", mockResolver);
+
+        verify(mockResolver, times(1)).registerError("a", "a is not a number");
+        verify(mockResolver, times(1)).registerError("c", "c is not less than or equals to 5");
+        verifyNoMoreInteractions(mockResolver);
+    }
+
+    @Test
     public void test_greaterThan() throws Exception {
         Validator v = Validators.greaterThan(5);
         v.validate("a", "a", mockResolver);
         v.validate("2", "b", mockResolver);
         v.validate("", "c", mockResolver);
+        v.validate("5", "d", mockResolver);
 
         verify(mockResolver, times(1)).registerError("a", "a is not a number");
         verify(mockResolver, times(1)).registerError("b", "b is not greater than 5");
+        verify(mockResolver, times(1)).registerError("d", "d is not greater than 5");
+        verifyNoMoreInteractions(mockResolver);
+    }
+
+
+    @Test
+    public void test_greaterThanOrEquals() throws Exception {
+        Validator v = Validators.greaterThanOrEquals(5);
+        v.validate("a", "a", mockResolver);
+        v.validate("2", "b", mockResolver);
+        v.validate("", "c", mockResolver);
+        v.validate("5", "d", mockResolver);
+
+        verify(mockResolver, times(1)).registerError("a", "a is not a number");
+        verify(mockResolver, times(1)).registerError("b", "b is not greater than or equals 5");
         verifyNoMoreInteractions(mockResolver);
     }
 }
